3b4c408d8412e424ed29aa3c33bf7b59ecf186db
abdala9512 <miguel.arquez12@gmail.com>
2025-07-14 19:07:57 -0500

    feat config

--- feast_service/fs_dsrp_mle2_jul9/feature_repo/bookings.py
+++ feast_service/fs_dsrp_mle2_jul9/feature_repo/bookings.py
@@ -6,6 +6,7 @@ from feast import (
     Field,
     RequestSource,
     FeatureService,
+    PushSource,
 )
 from feast.types import Int64, Float64, String
 from feast.on_demand_feature_view import on_demand_feature_view
@@ -16,16 +17,22 @@ booking = Entity(name="booking", join_keys=["booking_id"])
 
 booking_source = FileSource(
     name="booking_source",
-    path="./data/bookings_feature_table.parquet",
-    timestamp_field="timestamp",
+    path="data/bookings_feature_table.parquet",
+    timestamp_field="event_timestamp",
     created_timestamp_column="created",
 )
 
-booking_view = FeatureView(
-    name="booking",
+
+booking_push_source = PushSource(
+    name="booking_push_source",
+    batch_source=booking_source,
+)
+
+pc_booking_view = FeatureView(
+    name="pc_booking_view",
     entities=[booking],
+    online=True,
     schema=[
-        Field(name="booking_id", dtype=String),
         Field(name="great_feature1", dtype=Float64, description="This is a great feature"),
         Field(name="great_feature2", dtype=Float64, description="This is a great feature"),
     ],
@@ -35,23 +42,30 @@ booking_view = FeatureView(
 input_request = RequestSource(
     name="input_request",
     schema=[
-        Field(name="booking_id", dtype=String),
+        Field(name="kpi1", dtype=Float64),
+        Field(name="kpi2", dtype=Float64),
     ],
 )
 
 @on_demand_feature_view(
-    sources=[booking_view, input_request],
+    sources=[booking_push_source, input_request],
     schema=[
-        Field(name="great_feature1", dtype=Float64),
-        Field(name="great_feature2", dtype=Float64),
+        Field(name="great_feature1_kpi1", dtype=Float64),
+        Field(name="great_feature2_kpi2", dtype=Float64),
     ],
 )
-def great_feature_view(inputs: pd.DataFrame):
-    return pd.DataFrame(
-        {"great_feature1": [1.0, 2.0], "great_feature2": [3.0, 4.0]}
-    )
+def great_feature_view(inputs: pd.DataFrame) -> pd.DataFrame:
+    df = pd.DataFrame()
+    df["great_feature1_kpi1"] = inputs["great_feature1"] * inputs["kpi1"]
+    df["great_feature2_kpi2"] = inputs["great_feature2"] * inputs["kpi2"]
+    return df
 
 dsrp_feature_service = FeatureService(
     name="dsrp_feature_service",
-    features=[great_feature_view],
+    features=[booking_push_source,great_feature_view],
+)
+
+fs_service_pc = FeatureService(
+    name="fs_service_pc",
+    features=[pc_booking_view],
 )
