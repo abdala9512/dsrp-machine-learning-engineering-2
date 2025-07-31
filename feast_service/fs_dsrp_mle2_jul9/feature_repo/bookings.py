2087e309cc1cdb4d547631c959a7e9eef3f8e7ab
abdala9512 <miguel.arquez12@gmail.com>
2025-07-30 22:02:52 -0500

    embeddings + feast-mlflow

--- feast_service/fs_dsrp_mle2_jul9/feature_repo/bookings.py
+++ feast_service/fs_dsrp_mle2_jul9/feature_repo/bookings.py
@@ -10,7 +10,109 @@ from feast import (
 )
 from feast.types import Int64, Float64, String
 from feast.on_demand_feature_view import on_demand_feature_view
+
+import numpy as np
 import pandas as pd
+import uuid
+from datetime import datetime
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, OrdinalEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from sklearn.model_selection import train_test_split
+from sklearn.impute import KNNImputer, SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.decomposition import PCA
+from loguru import logger
+
+
+class FeatureEngineeringProcessor:
+
+    def __init__(self, raw_data: pd.DataFrame, pipeline_name: str) -> None:
+        self.raw_data = raw_data
+        self.pipeline_name = pipeline_name
+        self.feature_table = None
+
+
+    def impute_scale(self, n_components: int = 2) -> pd.DataFrame:
+        """Pipeline que imputa variables numericas y luego las escala, para finalmente
+        Aplicar PCA y quedarse con N componentes principales
+        """
+        numeric_cols= [
+            "lead_time",
+            "adults",
+            "children",
+            "babies",
+            "adr"
+            
+        ]
+        pipe = Pipeline(
+            steps=[
+                ("imputer_mean", SimpleImputer(strategy="mean")),
+                ("std_scaling", StandardScaler()),
+                ("pca", PCA(n_components=n_components))
+            ]
+        )
+        return pd.DataFrame(
+            pipe.fit_transform(self.raw_data[numeric_cols]),
+            columns=["great_feature1", "great_feature2"]
+        )
+        
+
+    def encode_categoricals(self) -> pd.DataFrame:
+
+        encoded_vars = []
+        for var in ["hotel", "market_segment", "reserved_room_type"]:
+            logger.info(f"Codificando con OHE {var}")
+            encoder = OneHotEncoder()
+            encoded = encoder.fit_transform(self.raw_data[[var]]).toarray()
+            cols  = [f"{var}_{col}" for col in encoder.categories_[0]]
+            _dataframe = pd.DataFrame(
+                encoded,
+                columns= cols
+            )
+            encoded_vars.append(_dataframe)
+        return pd.concat(encoded_vars,axis=1)
+
+
+    def run(self) -> pd.DataFrame:
+        # acá pondremos nuestro codigo
+        logger.info(f"Inicializando pipeline {self.pipeline_name}")
+
+        categorical = self.encode_categoricals()
+        numerics = self.impute_scale()
+
+        modeling_dataset = pd.concat([categorical, numerics], axis=1)
+        # Dataset Previo el pipeline
+        pipe = Pipeline(
+            steps=[
+                ("feature_selection", VarianceThreshold()),
+                ("scaling_robust", RobustScaler())
+            ]
+        )
+        self.feature_table =  pd.DataFrame(
+            pipe.fit_transform(modeling_dataset),
+            columns=modeling_dataset.columns
+        )
+        self.feature_table["booking_id"] = [str(uuid.uuid4()) for _ in range(self.feature_table.shape[0])]
+        self.feature_table["event_timestamp"] = [datetime.now() for _ in range(self.feature_table.shape[0])]
+        import time
+        time.sleep(1)
+        self.feature_table["created"] = [datetime.now() for _ in range(self.feature_table.shape[0])]
+
+        return self.feature_table
+
+    def write_feature_table(self, filepath: str) -> None:
+        """Escribimos la feature table final para modelamiento
+        """
+
+        if not self.feature_table.empty: # -> True o False
+            self.feature_table.to_parquet(f"{filepath}.parquet", index=False)
+            self.feature_table.to_csv(f"{filepath}.csv", index=False)
+        else:
+            raise Exception("La feature table no ha sido creada. Ejecutar el comando .run()")            
+        
+        
+        
 
 booking = Entity(name="booking", join_keys=["booking_id"])
 
